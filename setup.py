59a5b7d14dfba79889e937324a388862223ba17a
Chris Longo <chris.longo@gmail.com>
2011-12-13 15:00:21 -0500

    Better 2.6 setup

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import sys
+
 try:
     from ez_setup import use_setuptools
     use_setuptools()
@@ -9,32 +11,33 @@ try:
 except ImportError:
     from distutils.core import setup
 
-kwargs = {}
-
 __version__ = "0.5.0"
+
+if sys.version_info < (2, 6):
+    REQUIRES = ["pygments>=1.1.1", "argparse>=1.2.1"]
+else:
+    REQUIRES = ["pygments>=1.1.1"]
 
 setup(
     name="httpshell",
     version=__version__,
     packages=["httpshell"],
-    install_requires=['pygments'],
-    py_modules=['ez_setup'],
-    scripts=['httpsh'],
+    install_requires=REQUIRES,
+    py_modules=["ez_setup"],
+    scripts=["httpsh"],
     author="Chris Longo",
     author_email="chris.longo@gmail.com",
     url="https://github.com/chrislongo/HttpShell/",
     download_url="http://github.com/downloads/chrislongo/HttpShell/httpshell-%s.tar.gz" % __version__,
     description="An interactive shell for issuing HTTP commands to a web server or REST API",
       classifiers=[
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Natural Language :: English',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python :: 2.4',
-          'Programming Language :: Python :: 2.5',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-          'Topic :: System :: Networking'
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: MIT License",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: System :: Networking"
           ]
     )
